ba7e17dc4affce15f965cfdf58c5ad65542caa0d
Adam Gray <acgray@me.com>
2016-12-31 13:02:31 +0400

    Uncaught exception when first record in a buffer exceeds the buffer byte size limit (closes #11)

--- core/src/test/scala/com.snowplowanalytics.elasticsearch.loader/SnowplowElasticsearchEmitterSpec.scala
+++ core/src/test/scala/com.snowplowanalytics.elasticsearch.loader/SnowplowElasticsearchEmitterSpec.scala
@@ -16,6 +16,9 @@ package com.snowplowanalytics.elasticsearch.loader
 // Java
 import java.util.Properties
 
+// Scala
+import scala.collection.mutable.ListBuffer
+
 // AWS libs
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 
@@ -28,17 +31,11 @@ import com.amazonaws.services.kinesis.connectors.elasticsearch.ElasticsearchObje
 import com.amazonaws.services.kinesis.connectors.impl.BasicMemoryBuffer
 
 // Scala
-import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 
 // Scalaz
 import scalaz._
 import Scalaz._
-
-// json4s
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
-import org.json4s.JsonDSL._
 
 // Specs2
 import org.specs2.mutable.Specification
@@ -47,33 +44,159 @@ import org.specs2.mutable.Specification
 import sinks._
 import clients.ElasticsearchSender
 
+class MockElasticsearchSender extends ElasticsearchSender {
+  var sentRecords: List[EmitterInput] = List.empty
+  var callCount: Int = 0
+  val calls: ListBuffer[List[EmitterInput]] = new ListBuffer
+
+  override def sendToElasticsearch(records: List[EmitterInput]): List[EmitterInput] = {
+    sentRecords = sentRecords ::: records
+    callCount += 1
+    calls += records
+    List.empty
+  }
+  override def close() = {}
+  override def logClusterHealth(): Unit = ()
+  override val tracker = None
+}
+
 class SnowplowElasticsearchEmitterSpec extends Specification {
 
-  "The emitter method" should {
+  "The emitter" should {
     "return all invalid records" in {
 
       val fakeSender = new ElasticsearchSender {
-        override def sendToElasticsearch(records: List[EmitterInput]): List[EmitterInput] = records
+        override def sendToElasticsearch(records: List[EmitterInput]): List[EmitterInput] = List.empty
         override def close(): Unit = ()
         override def logClusterHealth(): Unit = ()
         override val tracker = None
       }
 
       val kcc = new KinesisConnectorConfiguration(new Properties, new DefaultAWSCredentialsProviderChain)
-      val eem = new SnowplowElasticsearchEmitter(kcc, Some(new StdouterrSink), new StdouterrSink,
-        fakeSender)
+      val eem = new SnowplowElasticsearchEmitter(kcc, None, new StdouterrSink, fakeSender)
 
       val validInput: EmitterInput = "good" -> new ElasticsearchObject("index", "type", "{}").success
       val invalidInput: EmitterInput = "bad" -> "malformed event".failureNel
 
       val input = List(validInput, invalidInput)
 
-      val bmb = new BasicMemoryBuffer[EmitterInput](kcc, input)
+      val bmb = new BasicMemoryBuffer[EmitterInput](kcc, input.asJava)
       val ub = new UnmodifiableBuffer[EmitterInput](bmb)
 
       val actual = eem.emit(ub)
 
       actual must_== List(invalidInput).asJava
+    }
+
+    "send multiple records in seperate requests where single record size > buffer bytes size" in {
+      val props = new Properties
+      props.setProperty(KinesisConnectorConfiguration.PROP_BUFFER_BYTE_SIZE_LIMIT, "1000")
+
+      val kcc = new KinesisConnectorConfiguration(props, new DefaultAWSCredentialsProviderChain)
+      val ess = new MockElasticsearchSender
+      val eem = new SnowplowElasticsearchEmitter(kcc, None, new StdouterrSink, ess)
+
+      val validInput: EmitterInput = "good" -> new ElasticsearchObject("index" * 10000, "type", "{}").success
+
+      val input = List.fill(50)(validInput)
+
+      val bmb = new BasicMemoryBuffer[EmitterInput](kcc, input.asJava)
+      val ub = new UnmodifiableBuffer[EmitterInput](bmb)
+
+      eem.emit(ub)
+
+      ess.sentRecords mustEqual input
+      ess.callCount mustEqual 50
+      forall (ess.calls) { c => c.length mustEqual 1 }
+    }
+
+    "send a single record in 1 request where record size > buffer bytes size " in {
+      val props = new Properties
+      props.setProperty(KinesisConnectorConfiguration.PROP_BUFFER_BYTE_SIZE_LIMIT, "1000")
+
+      val kcc = new KinesisConnectorConfiguration(props, new DefaultAWSCredentialsProviderChain)
+      val ess = new MockElasticsearchSender
+      val eem = new SnowplowElasticsearchEmitter(kcc, None, new StdouterrSink, ess)
+
+      val validInput: EmitterInput = "good" -> new ElasticsearchObject("index" * 10000, "type", "{}").success
+
+      val input = List(validInput)
+
+      val bmb = new BasicMemoryBuffer[EmitterInput](kcc, input.asJava)
+      val ub = new UnmodifiableBuffer[EmitterInput](bmb)
+
+      eem.emit(ub)
+
+      ess.sentRecords mustEqual input
+      ess.callCount mustEqual 1
+      forall (ess.calls) { c => c.length mustEqual 1 }
+    }
+
+    "send multiple records in 1 request where total byte size < buffer bytes size" in {
+      val props = new Properties
+      props.setProperty(KinesisConnectorConfiguration.PROP_BUFFER_BYTE_SIZE_LIMIT, "1048576")
+
+      val kcc = new KinesisConnectorConfiguration(props, new DefaultAWSCredentialsProviderChain)
+      val ess = new MockElasticsearchSender
+      val eem = new SnowplowElasticsearchEmitter(kcc, None, new StdouterrSink, ess)
+
+      val validInput: EmitterInput = "good" -> new ElasticsearchObject("index", "type", "{}").success
+
+      val input = List.fill(50)(validInput)
+
+      val bmb = new BasicMemoryBuffer[EmitterInput](kcc, input.asJava)
+      val ub = new UnmodifiableBuffer[EmitterInput](bmb)
+
+      eem.emit(ub)
+
+      ess.sentRecords mustEqual input
+      ess.callCount mustEqual 1
+      forall (ess.calls) { c => c.length mustEqual 50 }
+    }
+
+    "send a single record in 1 request where single record size < buffer bytes size" in {
+      val props = new Properties
+      props.setProperty(KinesisConnectorConfiguration.PROP_BUFFER_BYTE_SIZE_LIMIT, "1048576")
+
+      val kcc = new KinesisConnectorConfiguration(props, new DefaultAWSCredentialsProviderChain)
+      val ess = new MockElasticsearchSender
+      val eem = new SnowplowElasticsearchEmitter(kcc, None, new StdouterrSink, ess)
+
+      val validInput: EmitterInput = "good" -> new ElasticsearchObject("index", "type", "{}").success
+
+      val input = List(validInput)
+
+      val bmb = new BasicMemoryBuffer[EmitterInput](kcc, input.asJava)
+      val ub = new UnmodifiableBuffer[EmitterInput](bmb)
+
+      eem.emit(ub)
+
+      ess.sentRecords mustEqual input
+      ess.callCount mustEqual 1
+      forall (ess.calls) { c => c.length mustEqual 1 }
+    }
+
+    "send multiple records in batches where single record byte size < buffer size and total byte size > buffer size" in {
+      val props = new Properties
+      props.setProperty(KinesisConnectorConfiguration.PROP_BUFFER_BYTE_SIZE_LIMIT, "200")
+
+      val kcc = new KinesisConnectorConfiguration(props, new DefaultAWSCredentialsProviderChain)
+      val ess = new MockElasticsearchSender
+      val eem = new SnowplowElasticsearchEmitter(kcc, None, new StdouterrSink, ess)
+
+      // record size is 95 bytes
+      val validInput: EmitterInput = "good" -> new ElasticsearchObject("index", "type", "{}").success
+
+      val input = List.fill(20)(validInput)
+
+      val bmb = new BasicMemoryBuffer[EmitterInput](kcc, input.asJava)
+      val ub = new UnmodifiableBuffer[EmitterInput](bmb)
+
+      eem.emit(ub)
+
+      ess.sentRecords mustEqual input
+      ess.callCount mustEqual 10 // 10 buffers of 2 records each
+      forall (ess.calls) { c => c.length mustEqual 2 }
     }
   }
 
