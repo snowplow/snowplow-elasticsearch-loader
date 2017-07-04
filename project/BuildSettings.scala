09ff5b237b461fda21dc4d7efbaad66d9330f07e
Ben Fradet <benjamin.fradet@gmail.com>
2017-07-04 18:14:05 +0100

    Bump Scala version to 2.11 (closes #39)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -26,7 +26,7 @@ object BuildSettings {
     organization          :=  "com.snowplowanalytics",
     version               :=  "0.8.0",
     description           :=  "Kinesis sink for Elasticsearch",
-    scalaVersion          :=  "2.10.1",
+    scalaVersion          :=  "2.11.11",
     scalacOptions         :=  Seq("-deprecation", "-encoding", "utf8",
                                   "-feature", "-target:jvm-1.7"),
     scalacOptions in Test :=  Seq("-Yrangepos"),
