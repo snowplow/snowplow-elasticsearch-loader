725a448dcffe7852bf2a7b9ebb882c1457d877c8
Enes Aldemir <aldemirenes@live.com>
2017-08-16 14:23:46 +0300

    Support AT_TIMESTAMP as initial position (closes #63)

--- core/src/main/scala/com.snowplowanalytics.elasticsearch/loader/KinesisSourceExecutor.scala
+++ core/src/main/scala/com.snowplowanalytics.elasticsearch/loader/KinesisSourceExecutor.scala
@@ -20,12 +20,23 @@
 package com.snowplowanalytics
 package elasticsearch.loader
 
+// Logging
+import org.slf4j.LoggerFactory
+
 // AWS Kinesis Connector libs
 import com.amazonaws.services.kinesis.connectors.{
   KinesisConnectorConfiguration,
   KinesisConnectorExecutorBase,
   KinesisConnectorRecordProcessorFactory
 }
+
+// AWS Client Library
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker
+import com.amazonaws.services.kinesis.metrics.interfaces.IMetricsFactory
+
+// Java
+import java.util.Date
 
 // Tracker
 import snowplow.scalatracker.Tracker
@@ -42,6 +53,8 @@ import model._
  * @param documentIndex the elasticsearch index name
  * @param documentType the elasticsearch index type
  * @param config the KCL configuration
+ * @param initialPosition initial position for kinesis stream
+ * @param timestamp timestamp for "AT_TIMESTAMP" initial position
  * @param goodSink the configured GoodSink
  * @param badSink the configured BadSink
  * @param elasticsearchSender function for sending to elasticsearch 
@@ -52,13 +65,73 @@ class KinesisSourceExecutor(
   documentIndex: String,
   documentType: String,
   config: KinesisConnectorConfiguration,
+  initialPosition: String,
+  initialTimestamp: Option[Date],
   goodSink: Option[ISink],
   badSink: ISink,
   elasticsearchSender: ElasticsearchSender,
   tracker: Option[Tracker] = None
 ) extends KinesisConnectorExecutorBase[ValidatedRecord, EmitterInput] {
 
-  initialize(config)
+  val LOG = LoggerFactory.getLogger(getClass)
+
+  def getKCLConfig(initialPosition: String, timestamp: Option[Date], kcc: KinesisConnectorConfiguration): KinesisClientLibConfiguration = {
+      val cfg = new KinesisClientLibConfiguration(
+        kcc.APP_NAME,
+        kcc.KINESIS_INPUT_STREAM,
+        kcc.AWS_CREDENTIALS_PROVIDER,
+        kcc.WORKER_ID).withKinesisEndpoint(kcc.KINESIS_ENDPOINT)
+          .withFailoverTimeMillis(kcc.FAILOVER_TIME)
+          .withMaxRecords(kcc.MAX_RECORDS)
+          .withIdleTimeBetweenReadsInMillis(kcc.IDLE_TIME_BETWEEN_READS)
+          .withCallProcessRecordsEvenForEmptyRecordList(KinesisConnectorConfiguration.DEFAULT_CALL_PROCESS_RECORDS_EVEN_FOR_EMPTY_LIST)
+          .withCleanupLeasesUponShardCompletion(kcc.CLEANUP_TERMINATED_SHARDS_BEFORE_EXPIRY)
+          .withParentShardPollIntervalMillis(kcc.PARENT_SHARD_POLL_INTERVAL)
+          .withShardSyncIntervalMillis(kcc.SHARD_SYNC_INTERVAL)
+          .withTaskBackoffTimeMillis(kcc.BACKOFF_INTERVAL)
+          .withMetricsBufferTimeMillis(kcc.CLOUDWATCH_BUFFER_TIME)
+          .withMetricsMaxQueueSize(kcc.CLOUDWATCH_MAX_QUEUE_SIZE)
+          .withUserAgent(kcc.APP_NAME + ","
+            + kcc.CONNECTOR_DESTINATION + ","
+            + KinesisConnectorConfiguration.KINESIS_CONNECTOR_USER_AGENT)
+          .withRegionName(kcc.REGION_NAME)
+      
+      timestamp.filter(_ => initialPosition == "AT_TIMESTAMP")
+        .map(cfg.withTimestampAtInitialPositionInStream(_))
+        .getOrElse(cfg.withInitialPositionInStream(kcc.INITIAL_POSITION_IN_STREAM))
+  }
+
+  /**
+    * Initialize the Amazon Kinesis Client Library configuration and worker with metrics factory
+    * 
+    * @param kinesisConnectorConfiguration Amazon Kinesis connector configuration
+    * @param metricFactory would be used to emit metrics in Amazon Kinesis Client Library
+    */
+  override def initialize(kinesisConnectorConfiguration: KinesisConnectorConfiguration, metricFactory: IMetricsFactory): Unit = {
+    var kinesisClientLibConfiguration = getKCLConfig(initialPosition, initialTimestamp, kinesisConnectorConfiguration)
+
+    if (!kinesisConnectorConfiguration.CALL_PROCESS_RECORDS_EVEN_FOR_EMPTY_LIST) {
+      LOG.warn("The false value of callProcessRecordsEvenForEmptyList will be ignored. It must be set to true for the bufferTimeMillisecondsLimit to work correctly.")
+    }
+
+    if (kinesisConnectorConfiguration.IDLE_TIME_BETWEEN_READS > kinesisConnectorConfiguration.BUFFER_MILLISECONDS_LIMIT) {
+      LOG.warn("idleTimeBetweenReads is greater than bufferTimeMillisecondsLimit. For best results, ensure that bufferTimeMillisecondsLimit is more than or equal to idleTimeBetweenReads ")
+    }
+
+    // If a metrics factory was specified, use it.
+    worker = if (metricFactory != null) {
+      new Worker(getKinesisConnectorRecordProcessorFactory(),
+                 kinesisClientLibConfiguration,
+                 metricFactory)
+    } else {
+      new Worker(getKinesisConnectorRecordProcessorFactory(),
+                 kinesisClientLibConfiguration)
+    }
+    LOG.info(getClass().getSimpleName() + " worker created")
+  }
+  
+  initialize(config, null)
+
   override def getKinesisConnectorRecordProcessorFactory = {
     new KinesisConnectorRecordProcessorFactory[ValidatedRecord, EmitterInput](
       new KinesisElasticsearchPipeline(streamType, documentIndex, documentType, goodSink, badSink, elasticsearchSender, tracker), config)
