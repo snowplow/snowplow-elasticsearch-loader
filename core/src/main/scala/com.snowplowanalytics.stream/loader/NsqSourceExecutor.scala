cc604bd3102eec1cce96bb0305d86396dbb91b6c
Saeed Zareian <36857305+szareiangm@users.noreply.github.com>
2018-06-25 10:00:15 -0400

    Generalize package name for the project (closes #98)

---
+++ core/src/main/scala/com.snowplowanalytics.stream/loader/NsqSourceExecutor.scala
@@ -0,0 +1,123 @@
+/**
+  * Copyright (c) 2014-2017 Snowplow Analytics Ltd.
+  * All rights reserved.
+  *
+  * This program is licensed to you under the Apache License Version 2.0,
+  * and you may not use this file except in compliance with the Apache
+  * License Version 2.0.
+  * You may obtain a copy of the Apache License Version 2.0 at
+  * http://www.apache.org/licenses/LICENSE-2.0.
+  *
+  * Unless required by applicable law or agreed to in writing,
+  * software distributed under the Apache License Version 2.0 is distributed
+  * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+  * either express or implied.
+  *
+  * See the Apache License Version 2.0 for the specific language
+  * governing permissions and limitations there under.
+  */
+
+package com.snowplowanalytics.stream.loader
+
+// NSQ
+import com.snowplowanalytics.client.nsq.NSQConsumer
+import com.snowplowanalytics.client.nsq.lookup.DefaultNSQLookup
+import com.snowplowanalytics.client.nsq.NSQMessage
+import com.snowplowanalytics.client.nsq.NSQConfig
+import com.snowplowanalytics.client.nsq.callbacks.NSQMessageCallback
+import com.snowplowanalytics.client.nsq.callbacks.NSQErrorCallback
+import com.snowplowanalytics.client.nsq.exceptions.NSQException
+
+//Java
+import java.nio.charset.StandardCharsets.UTF_8
+
+// Scala
+import scala.collection.mutable.ListBuffer
+
+// Logging
+import org.slf4j.LoggerFactory
+
+// This project
+import sinks._
+import clients._
+import model._
+
+/**
+  * NSQSource executor
+  *
+  * @param streamType the type of stream, good, bad or plain-json
+  * @param documentIndex the elasticsearch index name
+  * @param documentType the elasticsearch index type
+  * @param nsq Nsq NsqConfig
+  * @param config ESLoader Configuration
+  * @param goodSink the configured GoodSink
+  * @param badSink the configured BadSink
+  * @param elasticsearchSender function for sending to elasticsearch
+  */
+class NsqSourceExecutor(
+                         streamType: StreamType,
+                         documentIndex: String,
+                         documentType: String,
+                         nsq: Nsq,
+                         config: ESLoaderConfig,
+                         goodSink: Option[ISink],
+                         badSink: ISink,
+                         elasticsearchSender: ElasticsearchSender
+                       ) extends Runnable {
+
+  lazy val log = LoggerFactory.getLogger(getClass())
+
+  // nsq messages will be buffered in msgBuffer until buffer size become equal to nsqBufferSize
+  private val msgBuffer = new ListBuffer[EmitterInput]()
+  // ElasticsearchEmitter instance
+  private val elasticsearchEmitter = new ElasticsearchEmitter(elasticsearchSender,
+                                                              goodSink,
+                                                              badSink,
+                                                              config.streams.buffer.recordLimit,
+                                                              config.streams.buffer.byteLimit)
+  private val transformer = streamType match {
+    case Good => new SnowplowElasticsearchTransformer(documentIndex, documentType)
+    case Bad => new BadEventTransformer(documentIndex, documentType)
+    case PlainJson => new PlainJsonTransformer(documentIndex, documentType)
+  }
+
+  /**
+    * Consumer will be started to wait new message.
+    */
+  override def run(): Unit = {
+    val nsqCallback = new NSQMessageCallback {
+      val nsqBufferSize = config.streams.buffer.recordLimit
+
+      override def message(msg: NSQMessage): Unit = {
+        val msgStr = new String(msg.getMessage(), UTF_8)
+        msgBuffer.synchronized {
+          val emitterInput = transformer.consumeLine(msgStr)
+          msgBuffer += emitterInput
+          msg.finished()
+
+          if (msgBuffer.size == nsqBufferSize) {
+            val elasticsearchRejects = elasticsearchEmitter.attemptEmit(msgBuffer.toList)
+            elasticsearchEmitter.fail(elasticsearchRejects)
+            msgBuffer.clear()
+          }
+        }
+      }
+    }
+
+    val errorCallback = new NSQErrorCallback {
+      override def error(e: NSQException): Unit =
+        log.error(s"Exception while consuming topic ${config.streams.inStreamName}", e)
+    }
+
+    // use NSQLookupd
+    val lookup = new DefaultNSQLookup
+    lookup.addLookupAddress(nsq.nsqlookupdHost, nsq.nsqlookupdPort)
+    val consumer = new NSQConsumer(lookup,
+      config.streams.inStreamName,
+      nsq.channelName,
+      nsqCallback,
+      new NSQConfig(),
+      errorCallback)
+    consumer.start()
+  }
+}
---
+++ core/src/main/scala/com.snowplowanalytics.stream/loader/PlainJsonTransformer.scala
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2014-2017 Snowplow Analytics Ltd.
+ * All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache
+ * License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the Apache License Version 2.0 for the specific language
+ * governing permissions and limitations there under.
+ */
+
+package com.snowplowanalytics.stream.loader
+
+// Java
+import java.nio.charset.StandardCharsets.UTF_8
+import java.util.UUID
+
+// Amazon
+import com.amazonaws.services.kinesis.connectors.interfaces.ITransformer
+import com.amazonaws.services.kinesis.connectors.elasticsearch.ElasticsearchObject
+import com.amazonaws.services.kinesis.model.Record
+
+// json4s
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+// Scalaz
+import scalaz._
+import Scalaz._
+
+/**
+ * Class to convert plain JSON to EmitterInputs
+ *
+ * @param documentIndex the elasticsearch index name
+ * @param documentType the elasticsearch index type
+ */
+class PlainJsonTransformer(documentIndex: String, documentType: String)
+  extends ITransformer[ValidatedRecord, EmitterInput] with StdinTransformer {
+
+  /**
+   * Convert an Amazon Kinesis record to a json string
+   *
+   * @param record Byte array representation of an enriched event string
+   * @return ValidatedRecord for the event
+   */
+  override def toClass(record: Record): ValidatedRecord = {
+    val recordString = new String(record.getData.array, UTF_8)
+    (recordString, toJsonRecord(recordString))
+  }
+
+  /**
+   * Convert plain json to an EmitterInput
+   *
+   * @param record ValidatedRecord containing plain JSON
+   * @return An EmitterInput
+   */
+  override def fromClass(record: ValidatedRecord): EmitterInput = {
+    val uuid = UUID.randomUUID().toString // generate unique UUID for every Json document
+    record.map(_.map(r => new ElasticsearchObject(documentIndex, documentType, uuid, r.json)))
+  }
+
+  /**
+   * Parses a json string as a JsonRecord.
+   *
+   * @param jsonString the JSON string to be parsed
+   * @return the parsed JsonRecord
+   */
+  private def toJsonRecord(jsonString: String): ValidationNel[String, JsonRecord] = {    
+    parseOpt(jsonString) match {
+      case Some(jvalue) => JsonRecord(jsonString, None).success 
+      case None => "Json parsing error".failureNel
+    }
+  }
+
+  /**
+   * Consume data from stdin/NSQ rather than Kinesis
+   *
+   * @param line Line from stdin/NSQ
+   * @return Line as an EmitterInput
+   */
+  override def consumeLine(line: String): EmitterInput =
+    fromClass(line -> toJsonRecord(line))
+
+}
