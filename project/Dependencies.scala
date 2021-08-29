c6a49f082b39555729604312c01f958602695d4c
Ian Streeter <ian@snowplowanalytics.com>
2021-08-29 21:01:55 +0100

    Bump slf4j to 1.7.32 (close #184)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   object V {
     // Java
     val config           = "1.4.1"
-    val slf4j            = "1.7.5"
+    val slf4j            = "1.7.32"
     val log4j            = "2.11.1"
     val kinesisClient    = "1.9.1"
     val kinesisConnector = "1.3.0"
